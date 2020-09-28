56bd1b8814dc6c81e70b2572815ba0457ac921f6
Reilly Knutson <Reillyknut@gmail.com>
2020-09-28 16:10:44 -0400

    Adding exercise files

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -1,31 +1,107 @@
-# ask user for two numbers
-# ask user for the type of operation to perform: add, subtract, multiply or divide
-# perform the requested operation on the two numbers
-# display the result
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
 
-
-puts "Welcome to my calculator."
-
-puts "Please type in a number:"
-num1 = gets.chomp
-
-puts "Please type in a second number:"
-num2 = gets.chomp
-
-puts "What would you like to to with these numbers? (1:add 2:subtract 3:multiply 4:divide)"
-operator = gets.chomp
-
-if operator == '1'
-  result = num1.to_i + num2.to_i
-
-elsif operator == '2'
-  result = num1.to_i - num2.to_i
-
-elsif operator == '3'
-  result = num1.to_i * num2.to_i
-
-else operator == '4'
-  result = num1.to_f / num2.to_f
+def prompt(message)
+  puts("=> #{message}")
 end
 
-puts "The result is #{result}"
+def valid_number?(num)
+  num.to_i.to_s == num
+end
+
+def operation_to_message(op)
+  case op
+  when '1'
+    'Adding'
+  when '2'
+    'Subtracting'
+  when '3'
+    'Multiplying'
+  when '4'
+    'Dividing'
+  end
+  op
+end
+
+prompt(MESSAGES['welcome'])
+
+yourname = ''
+loop do
+  yourname = gets.chomp
+
+  if yourname.empty?
+    prompt(MESSAGES['valid_name'])
+  else
+    break
+  end
+end
+
+prompt("Hi #{yourname}!")
+
+loop do # main loop
+  num1 = ''
+  loop do
+    prompt(MESSAGES['number1'])
+    num1 = gets.chomp
+
+    if valid_number?(num1)
+      break
+    else
+      prompt(MESSAGES['valid_number'])
+    end
+  end
+
+  num2 = ''
+  loop do
+    prompt(MESSAGES['number2'])
+    num2 = gets.chomp
+
+    if valid_number?(num2)
+      break
+    else
+      prompt(MESSAGES['valid_number'])
+    end
+  end
+
+  operator_prompt = <<-MSG
+    What operation would you like to perform?
+    1) add
+    2) subtract
+    3) multiply
+    4) divide
+  MSG
+
+  prompt(operator_prompt)
+
+  operator = ''
+  loop do
+    operator = gets.chomp
+
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt(MESSAGES['valid_operation'])
+    end
+  end
+
+  prompt("#{operation_to_message(operator)} the two numbers...")
+
+  result = case operator
+           when '1'
+             num1.to_i + num2.to_i
+           when '2'
+             num1.to_i - num2.to_i
+           when '3'
+             num1.to_i * num2.to_i
+           when '4'
+             num1.to_f / num2.to_f
+           end
+
+  prompt("The result is #{result}")
+
+  prompt(MESSAGES['try_again?'])
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt(MESSAGES['end'])
---
+++ lesson_2/mortgage_calculator.rb
@@ -0,0 +1,68 @@
+def prompt(words)
+  puts "***** #{words}"
+end
+
+def float?(num)
+  num.to_f.to_s == num
+end
+
+def integer?(num)
+  num.to_i.to_s == num
+end
+
+def valid_number?(num)
+  float?(num) || integer?(num)
+end
+
+prompt("Welcome to my mortgage/car loan calculator!")
+
+loop do # start of main loop
+  prompt("Enter the total dollar amount of the loan:")
+  loan_amount = ''
+  loop do
+    loan_amount = gets.chomp
+
+    if loan_amount.to_f > 0 && valid_number?(loan_amount)
+      break
+    else
+      prompt("Invalid number. Try again.")
+    end
+  end
+
+  prompt("Enter the annual percentage rate (in decimal form):")
+  apr = ''
+  loop do
+    apr = gets.chomp
+    if apr.to_f <= 1 && valid_number?(apr)
+      break
+    else
+      prompt("Invalid number. Try again.")
+    end
+  end
+
+  prompt("Enter the duration of the loan in years:")
+  loan_duration_years = ''
+  loop do
+    loan_duration_years = gets.chomp
+    if loan_duration_years.to_f >= 0 && valid_number?(loan_duration_years)
+      break
+    else
+      prompt("Invalid number. Try again.")
+    end
+  end
+
+  duration_months = loan_duration_years.to_i * 12
+  prompt("Your loan duration in months is #{duration_months}")
+
+  month_interest = apr.to_f / 12
+  prompt("Your monthly interest rate is #{month_interest.round(4) * 100}%")
+
+  month_pay = loan_amount.to_f * (month_interest / (1 - (1 + month_interest)**(-duration_months)))
+  prompt("Your monthly payment for this loan is $#{month_pay.round(2)}")
+
+  prompt("Want to perform another calculation? (Y to calculate again)")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt("Thank you for using my calculator! Goodbye!")
